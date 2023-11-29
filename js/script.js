38c897a5bd359105542ea752b016753bbafe125f
Medyk Gnggul <medykgonggul@.com>
2023-11-29 16:01:59 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika dokumen hamburger di klik
+document.querySelector('#hamburger-menu').onclik= () => {
+   navbarNav,classlist. toggle('active');
+};
